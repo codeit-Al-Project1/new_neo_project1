e57eb55e40844979aa841e9bff547c8104d13e40
Glen0227 <keugmo@gmail.com>
2025-03-21 10:48:28 +0900

    전체 상황 저장

--- src/model_utils/basic_frcnn.py
+++ src/model_utils/basic_frcnn.py
@@ -14,7 +14,6 @@ def get_fast_rcnn_model(num_classes):
 
     return model
 
-
 def save_model(model, save_dir="./models", base_name="model", ext=".pth"):
     """
     모델을 저장할 때, 기존 파일 개수를 기준으로 자동 넘버링하여 저장하는 함수.
@@ -28,7 +27,6 @@ def save_model(model, save_dir="./models", base_name="model", ext=".pth"):
     Returns:
         str: 저장된 파일의 전체 경로
     """
-
     os.makedirs(save_dir, exist_ok=True)
     
     existing_models = [f for f in os.listdir(save_dir) if f.startswith(base_name) and f.endswith(ext)]
@@ -37,7 +35,6 @@ def save_model(model, save_dir="./models", base_name="model", ext=".pth"):
 
     model_save_path = os.path.join(save_dir, f"{base_name}_{model_count}{ext}")
     torch.save(model.state_dict(), model_save_path)
-
     print(f"Model saved to {model_save_path}")
     return model_save_path
 
